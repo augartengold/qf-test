5f565c566f5f83494e83593e0282d3c83ad4bd3a
tindli <officemab@gmail.com>
2021-02-04 22:33:37 +0100

    add changes

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 
 // Demo inspired by https://github.com/arvydas/blinkstick-node
 
-const vendorId = 0x0403; // FTDI
-let device;
+const vendorId = 0x0403; // FTDI, Apple: 0x05ac
+
 
 document.querySelector('button').addEventListener('click', handleClick);
 
 async function handleClick() {
     const device = await getOpenedDevice();
-    writeVal()
+    writeVal(device)
 }
 
 async function getOpenedDevice() {
-    const devices = await navigator.hid.getDevices();
-    device = devices.find(d => d.vendorId === vendorId);
+    //const devices = await navigator.hid.getDevices();
+    //device = devices.find(d => d.vendorId === vendorId);
 
-    if (!device) {
+    //if (!device) {
         device = await navigator.hid.requestDevice({filters: [{ vendorId }],});
-    }
+    //}
+
+    console.log(device)
+    device = device[0]
 
     if (!device.opened) {
+        console.log("OPENING")
         await device.open();
     }
 
+    console.log(device)
+    console.log(device.collections)
     return device;
 }
 
-async function writeVal() {
+async function writeVal(device) {
    
     const reportId = 0xF0;
     const len = 4
     const data = Int8Array.from([reportId, len, 0x55, 1, 2, 3]);
-  
+
     try {
         // Request feature report.
-        const dataView = await device.receiveFeatureReport(/* reportId= */ 0xE0);
+        console.log("sending feature request")
+        const dataView = await device.receiveFeatureReport(/* reportId= */ 0xA0);
         console.log(dataView);
     } catch (error) {
         console.log("ERROR");
